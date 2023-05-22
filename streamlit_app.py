2a85ce8c6b8bec5eb00b1519d4d9296d32bad3da
BillaNaveen <134215583+BillaNaveen@users.noreply.github.com>
2023-05-22 06:58:34 +0530

    Update streamlit_app.py
    Added symbols to the code breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header(' Break fast menu')
-streamlit.text('omega3 & blueberry oatmeal ')
-streamlit.text('Kale, Spinach & Rocket Smoothie ')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥣 omega3 & blueberry oatmeal ')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie ')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
